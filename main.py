8f5197be130103ec82028d8475d2879ef8dc76b5
Ry0nan <125460295+Ry0nan@users.noreply.github.com>
2023-10-17 10:14:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+
+def get_driver():
+  #Set options to make browsing Easier
+  options = webdriver.ChromeOptions()
+  options.add_argument("disable-infobars")
+  options.add_argument("start-maximized")
+  options.add_argument("disable-dev-shm-usage")
+  options.add_argument("no-sandbox")
+  options.add_experimental_option("excludeSwitches",
+                                 ["enable-automation"])
+  options.add_argument("disable-blink-feaututes=AutomationControlled")
+  
+  driver = webdriver.Chrome(options)
+  driver.get(https://automated.pythonanywhere.com/)
+  return driver
